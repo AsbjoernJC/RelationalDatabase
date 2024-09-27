cd70eacc55ab9a93f72ac4acaacea7b19a90999e
AsbjoernJC <asbjoernjc@gmail.com>
2024-09-27 17:55:44 +0200

    Updated sh

--- NumberGuessingGames/number_guessing_game/number_guess.sh
+++ NumberGuessingGames/number_guessing_game/number_guess.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
-$($PSQL "TRUNCATE TABLE users, games, usergames")
+# $PSQL "TRUNCATE TABLE usergames, users, games"
 # Generate a random number between 1 and 1000
 RANDOM_NUMBER=$((RANDOM % 1001))
+GUESSED_NUMBER=-1
 
 # Returns name and user_id
 GET_USER() {
-  echo -e "$($PSQL "SELECT name, user_id FROM users WHERE name=$'$1'")"
+  echo -e "$($PSQL "SELECT name, user_id FROM users WHERE name='$1'")"
 }
 
 # Returns number of games a user has played
 GET_GAMES_PLAYED() {
-  echo -e "$($PSQL "SELECT count(*) AS num_games FROM usergames WHERE user_id=$1 GROUP BY user_id ")"
+  echo -e "$($PSQL "SELECT count(*) AS num_games FROM usergames WHERE user_id=$1 GROUP BY user_id")"
 }
 
 # Returns the least number of guess a user has used in a game
@@ -21,10 +22,12 @@ GET_BEST_GAME() {
 
 # Returns the least number of guess a user has used in a game
 GET_GAME_INFO() {
-  echo "$1" | while read USERNAME BAR USER_ID
+  USER_ID=-1
+  echo "$1" | while IFS='|' read USERNAME LOC_USER_ID
   do
-    GAMES_PLAYED=$(GET_GAMES_PLAYED $USER_ID)
-    BEST_GAME=$(GET_BEST_GAME $USER_ID)
+    USER_ID=$LOC_USER_ID
+    GAMES_PLAYED=$(GET_GAMES_PLAYED $LOC_USER_ID)
+    BEST_GAME=$(GET_BEST_GAME "$LOC_USER_ID")
     echo -e "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   done
 }
@@ -33,20 +36,21 @@ GET_GAME_INFO() {
 GET_USERINPUT_INT() {
     while :
     do
-        READ GUESS
-        echo "That is not an integer, guess again:"
+        read GUESS
         if [[ ! $GUESS =~ ^[0-9]+$ ]]; 
         then
-            break
+          echo "That is not an integer, guess again:"
+        else
+          break
         fi
     done
-    echo $GUESS
+    GUESSED_NUMBER=$GUESS 
 }
 
 #Returns user id
 GET_USER_ID() {
   GET_USER_ID_RESULT=$($PSQL "SELECT user_id FROM users WHERE name='$1'")
-  ceho -e "$GET_USER_ID_RESULT"
+  echo -e "$GET_USER_ID_RESULT"
 }
 
 #Returns 
@@ -72,45 +76,50 @@ INSERT_NEW_GAME() {
 }
 
 INSERT_NEW_USERGAME() {
-  INSERT_USERGAME_RESULT=$($PSQL "INSERT INTO games(game_id, user_id, guesses) VALUES($GAME_ID, $1, $2)")
+  INSERT_USERGAME_RESULT=$($PSQL "INSERT INTO usergames(game_id, user_id, guesses) VALUES($GAME_ID, $1, $2)")
+}
+
+GREET_NEW_USER() {
+  echo "Welcome, $1! It looks like this is your first time here."
 }
 
 
 # The main game loop
 GUESSING_GAME_LOOP() {
     USER_ID=$1
-    GAME_ID=$( GET_GAME_ID )
-    echo Guess the secret number between 1 and 1000:
-    GUESSED_NUMBER=-1
+    echo "Guess the secret number between 1 and 1000:"
     NUMBER_GUESSES=0
     while [[ $GUESSED_NUMBER != $RANDOM_NUMBER ]]
     do
-        if [[ $NUMBER_GUESSES != 0 && $GUESSED_NUMBER < $RANDOM_NUMBER ]]
+        if [[ $NUMBER_GUESSES != 0 && $GUESSED_NUMBER -gt $RANDOM_NUMBER ]]
         then
             echo "It's lower than that, guess again:"
-        elif [[ $NUMBER_GUESSES != 0 && $GUESSED_NUMBER > $RANDOM_NUMBER ]]
+        elif [[ $NUMBER_GUESSES != 0 && $GUESSED_NUMBER -lt $RANDOM_NUMBER ]]
         then
             echo "It's higher than that, guess again:"
         fi
-        GUESSED_NUMBER=$(GET_USERINPUT_INT)
+        GET_USERINPUT_INT
         NUMBER_GUESSES=$(($NUMBER_GUESSES + 1))
     done
     echo "You guessed it in $NUMBER_GUESSES tries. The secret number was $RANDOM_NUMBER. Nice job!"
-    INSERT_NEW_USERGAME $USER_ID
+    INSERT_NEW_GAME $RANDOM_NUMBER
+    GAME_ID=$( GET_GAME_ID )
+    INSERT_NEW_USERGAME $USER_ID $NUMBER_GUESSES
 }
 
 MAIN() {
     echo "Enter your username:"
     read USERNAME
-    INSERT_NEW_GAME $RANDOM_NUMBER
     GET_USER_RESULT=$(GET_USER "$USERNAME")
-    echo $GET_USER_RESULT
     if [[ -z $GET_USER_RESULT ]] 
     then
         INSERT_NEW_USER "$USERNAME"
-        GUESSING_GAME_LOOP "$USERNAME"
+        GREET_NEW_USER "$USERNAME"
+        GUESSING_GAME_LOOP "$(GET_USER_ID "$USERNAME")"
     else
         GET_GAME_INFO "$GET_USER_RESULT"
+        USER_ID= "$(GET_USER_ID "$USERNAME")"
+        echo RETURNING USER $USER_ID
         GUESSING_GAME_LOOP $USER_ID
     fi
 
